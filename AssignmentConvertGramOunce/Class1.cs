0dcf53201e411b4ea4b5c3610b8e6c7e70ae5122
Ahsan Javaid Iqbal <ahsa0005@edu.easj.dk>
2018-09-27 17:50:41 +0200

    Initial commit

---
+++ AssignmentConvertGramOunce/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AssignmentConvertGramOunce
+{
+    public class Class1
+    {
+    }
+}
---
+++ AssignmentConvertGramOunce/Data/Conversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentConvertGramOunce.Data
+{
+    class Conversion
+    {
+        public double ConvertToOunces(double a)
+        {
+            
+           return a/28.34952;
+        }
+
+        public double ConvertToGram(double a)
+        {
+            return a * 28.34952;
+        }
+
+
+
+    }
+}
